3a1c5e1b4b78ee1bbc1a23afba7d8228879f1283
Jossy2005 <jab.mena@yavirac.edu.ec>
2024-11-15 21:56:09 -0500

    Database provider

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 
 async function bootstrap() {
@@ -8,5 +9,16 @@ async function bootstrap() {
   //class validator
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
+
+  //swagger
+  const config = new DocumentBuilder()
+  .setTitle('Cats examples')
+  .setDescription('The prouct API description')
+  .setVersion('1.0')
+  .addTag('product')
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document);
+await app.listen(3000);
 }
 bootstrap();
