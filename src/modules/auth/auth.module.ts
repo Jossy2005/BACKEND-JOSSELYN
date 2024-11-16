a386b2978aadadbc1f293766ea0b05042d49a2cc
Jossy2005 <jab.mena@yavirac.edu.ec>
2024-11-15 19:12:57 -0500

    Clase7

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,15 +2,18 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
+
+
 
 @Module({
   imports:[
     JwtModule.register({
-      secret: "MI NUEVO CÓIGO BEBÉ",
+      secret: "MI NUEVO CÓDIGO BEBÉ",
       signOptions:{expiresIn:'605'}
     })
   ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, JwtStrategy]
 })
 export class AuthModule {}
